59f25ea0ff50333c719f921f1a695931c418b141
GeorgianaIonUPD <georgiana.ion@updivision.com>
2024-08-13 16:28:51 +0300

    clean-up

--- Assets/Scripts/AvgTournamentsLeaderboard.cs
+++ Assets/Scripts/AvgTournamentsLeaderboard.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Beamable;
 using Beamable.Api;
 using Beamable.Common.Api.Tournaments;
-using Beamable.Server.Clients;
-using Managers;
 using UnityEngine;
 using TMPro;
 
@@ -19,13 +17,9 @@ public class AvgTournamentLeaderboard : MonoBehaviour
 
     private async void Start()
     {
-        Debug.Log("Starting TournamentScript...");
-
         // Initialize BeamContext and services
         _beamContext = await BeamContext.Default.Instance;
-
-        Debug.Log("BeamContext and services initialized.");
-
+        
         // Get the active or upcoming tournament
         var activeTournament = await GetActiveOrUpcomingTournament();
         if (activeTournament == null)
@@ -33,12 +27,9 @@ public class AvgTournamentLeaderboard : MonoBehaviour
             Debug.LogError("No active or upcoming tournament found.");
             return;
         }
-
-        Debug.Log($"Active Tournament found: ID={activeTournament.tournamentId}, Start Time={activeTournament.startTimeUtc}, End Time={activeTournament.endTimeUtc}");
-
+        
         // Construct the group average leaderboard ID
         var groupAvgLeaderboardId = ConstructGroupAvgLeaderboardId(activeTournament.tournamentId);
-        Debug.Log($"Constructed Group Average Leaderboard ID: {groupAvgLeaderboardId}");
 
         // Display the leaderboard
         await DisplayGroupLeaderboard(groupAvgLeaderboardId);
@@ -46,16 +37,13 @@ public class AvgTournamentLeaderboard : MonoBehaviour
 
     private async Task<TournamentInfo> GetActiveOrUpcomingTournament()
     {
-        Debug.Log("Fetching active or upcoming tournaments...");
         var tournaments = await GetTournaments();
 
         // Filter to get active and future tournaments
         var validTournaments = tournaments.Where(t =>
         {
-            DateTime startTime;
-            DateTime endTime;
-            bool isStartTimeValid = DateTime.TryParse(t.startTimeUtc, out startTime);
-            bool isEndTimeValid = DateTime.TryParse(t.endTimeUtc, out endTime);
+            var isStartTimeValid = DateTime.TryParse(t.startTimeUtc, out _);
+            var isEndTimeValid = DateTime.TryParse(t.endTimeUtc, out var endTime);
 
             // Consider tournaments that are either currently active or will start in the future
             return isStartTimeValid && isEndTimeValid && endTime > DateTime.UtcNow;
@@ -70,7 +58,6 @@ public class AvgTournamentLeaderboard : MonoBehaviour
                 return endTime;
             }).FirstOrDefault();
 
-            Debug.Log($"Valid Tournament found: ID={tournamentWithLatestEndTime.tournamentId}, End Time={tournamentWithLatestEndTime.endTimeUtc}");
             return tournamentWithLatestEndTime;
         }
 
@@ -80,28 +67,22 @@ public class AvgTournamentLeaderboard : MonoBehaviour
 
     private async Task<List<TournamentInfo>> GetTournaments()
     {
-        Debug.Log("Fetching all tournaments...");
         var response = await _beamContext.Api.TournamentsService.GetAllTournaments();
         var tournaments = response?.tournaments ?? new List<TournamentInfo>();
-
-        Debug.Log($"Total tournaments fetched: {tournaments.Count}");
         return tournaments;
     }
 
     private string ConstructGroupAvgLeaderboardId(string tournamentId)
     {
         var leaderboardId = $"{tournamentId}.0.0.group#0";
-        Debug.Log($"Constructed Group Average Leaderboard ID: {leaderboardId}");
         return leaderboardId;
     }
 
     private async Task DisplayGroupLeaderboard(string leaderboardId)
     {
-        Debug.Log($"Displaying Group Leaderboard for ID: {leaderboardId}");
         try
         {
             var view = await _beamContext.Api.LeaderboardService.GetBoard(leaderboardId, 1, 1000);
-            Debug.Log($"Leaderboard fetched: {view.rankings.Count} entries found.");
 
             foreach (Transform child in scrollViewContent)
             {
@@ -111,7 +92,6 @@ public class AvgTournamentLeaderboard : MonoBehaviour
             foreach (var rankEntry in view.rankings)
             {
                 var groupName = await GetGroupName(rankEntry.gt);
-                Debug.Log($"Group Rank Entry: Group={groupName}, Score={rankEntry.score}");
                 CreateRankingItem(groupName, rankEntry.score.ToString());
             }
         }
@@ -127,17 +107,14 @@ public class AvgTournamentLeaderboard : MonoBehaviour
 
     private async Task<string> GetGroupName(long groupId)
     {
-        Debug.Log($"Fetching group name for Group ID: {groupId}");
         try
         {
             var group = await _beamContext.Api.GroupsService.GetGroup(groupId);
             if (group != null)
             {
-                Debug.Log($"Group name retrieved: {group.name}");
                 return group.name;
             }
 
-            Debug.LogWarning($"Group name not found. Returning Group ID as name.");
             return groupId.ToString();
         }
         catch (Exception e)
@@ -149,7 +126,6 @@ public class AvgTournamentLeaderboard : MonoBehaviour
 
     private void CreateRankingItem(string groupName, string score)
     {
-        Debug.Log($"Creating ranking item: Group={groupName}, Score={score}");
         GameObject rankingItem = Instantiate(rankingItemPrefab, scrollViewContent);
         TextMeshProUGUI[] texts = rankingItem.GetComponentsInChildren<TextMeshProUGUI>();
 
